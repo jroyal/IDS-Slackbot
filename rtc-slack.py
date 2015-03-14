a4efd85e77bc810d072c65c367fddd41e92a1b91
James Royal <jhr.atx@gmail.com>
2015-03-14 17:53:08 -0500

    Will correctly send a message to slack when finding a single work item.

---
+++ rtc-slack.py
@@ -0,0 +1,45 @@
+__author__ = 'jhroyal'
+
+from rtcclient import RTCClient
+import requests
+import json
+import yaml
+
+
+def send_message_to_slack(url, workitem):
+    payload = {
+        "attachments": [
+            {
+                "fallback": "<%s|%s %s> has been updated!" % (workitem.url, workitem.type, workitem.id),
+                "pretext": "A workitem has been updated!",
+                "color": "good",
+                "fields": [{
+                               "title": "Update",
+                               "value": "<%s|%s %s: %s> \nState has been changed to %s" % (
+                               workitem.url, workitem.type, workitem.id, workitem.summary, workitem.state),
+                               "short": False
+                           }
+                ]
+            }
+        ]
+    }
+    requests.post(url, data=json.dumps(payload))
+
+
+def load_env(self):
+    pass
+
+
+def test(env):
+    rtc = RTCClient(env)
+    workitem = rtc.get_work_item(43746)
+    send_message_to_slack(env["SLACK_URL"], workitem)
+
+
+def test2(env):
+    rtc = RTCClient(env)
+    rtc.get_filtered_results()
+
+if __name__ == '__main__':
+    env = yaml.load(file("env.yaml"))
+    test(env)
--- rtcclient.py
+++ rtcclient.py
@@ -1,21 +1,63 @@
 __author__ = 'jhroyal'
 import requests
 import json
-import xmltodict
+
+
+class RTCWorkItem():
+    def __init__(self, obj_json):
+        self.summary = obj_json["dc:title"]
+        self.id = obj_json["dc:identifier"]
+        type_string = obj_json["dc:type"]["rdf:resource"]
+        self.type = type_string[type_string.rfind("/") + 1:].title()
+        self.state = self.get_state(obj_json["rtc_cm:state"]["rdf:resource"])
+        self.url = obj_json["rdf:resource"]
+
+    def get_state(self, url):
+        # states: idea = new, defined = in progress, tested=implemented, state.s2=invalid, verified=done
+        value = "Unknown State"
+        if "story" in url:
+            if "idea" in url:
+                value = "New"
+            elif "defined" in url:
+                value = "In Progress"
+            elif "tested" in url:
+                value = "Implemented"
+            elif "verified" in url:
+                value = "Done"
+            elif "state.s2" in url:
+                value = "Invalid"
+            else:
+                value = "Unknown State"
+        elif "task" in url:
+            state = url[url.rfind("/") + 1:]
+            if state == "1":
+                value = "New"
+            elif state == "2":
+                value = "In Progress"
+            elif state == "3":
+                value = "Done"
+
+        return value
+
+    def __str__(self):
+        return """%s %s
+    Summary: %s
+    State: %s
+        """ % (self.type, self.id, self.summary, self.state)
 
 
 class RTCClient(object):
-    def __init__(self):
-        self.base_url = "https://hub.jazz.net/ccm08"
+    def __init__(self, env):
+        self.base_url = env["JAZZ_URL"]
+        self.jazz_user = env["JAZZ_USERNAME"]
+        self.jazz_pass = env["JAZZ_PASSWORD"]
+        self.projectarea = env["PROJECT_AREA"]
         auth_uri = "/authenticated/identity"
-        jazz_user = "username"
-        jazz_pass = "password"
-
         self.session = requests.Session()
         self.session.verify = False
         self.session.allow_redirects = True
-        self.session.headers = {'accept':'application/json'}
-        self.session.auth = (jazz_user, jazz_pass)
+        self.session.headers = {'accept': 'application/json'}
+        self.session.auth = (self.jazz_user, self.jazz_pass)
 
         print "Request for authenticated resource"
         response = self.session.get(self.base_url + auth_uri, verify=False)
@@ -27,7 +69,8 @@ class RTCClient(object):
 
             # Form response
             print "Sending login POST"
-            login_response = self.session.post(self.base_url + '/j_security_check', data={ 'j_username': jazz_user, 'j_password': jazz_pass } )
+            login_response = self.session.post(self.base_url + '/j_security_check',
+                                               data={'j_username': self.jazz_user, 'j_password': self.jazz_pass})
             print login_response.headers
 
             if 'x-com-ibm-team-repository-web-auth-msg' in login_response.headers and login_response.headers['x-com-ibm-team-repository-web-auth-msg'] == 'authrequired':
@@ -41,11 +84,16 @@ class RTCClient(object):
             print response.headers
             print response.text
 
-    def getWorkItem(self, itemNumber):
-        #original_url = "/rpt/repository/workitem?fields=workitem/workItem[id=%s]/(*)" % itemNumber
-        old_url = "/oslc/workitems/%s.xml" %itemNumber
+    def get_work_item(self, itemNumber):
+        old_url = "/oslc/workitems/%s.json?oslc_cm.properties=dc:identifier,dc:title,rtc_cm:state,rtc_cm:ownedBy,dc:type" % itemNumber
         response = self.session.get(self.base_url + old_url, verify=False)
         print response.headers
-        print response.text
-        output = xmltodict.parse(response.text)
-        print json.dumps(output, indent=4, sort_keys=True)
+        print json.dumps(json.loads(response.text), indent=4, sort_keys=True)
+        return RTCWorkItem(json.loads(response.text))
+
+
+    def get_filtered_results(self):
+        url = "/oslc/contexts/%s/workitems.json?oslc_cm.properties=dc:identifier,dc:title,rtc_cm:state,rtc_cm:ownedBy,dc:type" % self.projectarea
+        response = self.session.get(self.base_url + url, verify=False)
+        print response.headers
+        print json.dumps(json.loads(response.text), indent=4, sort_keys=True)
